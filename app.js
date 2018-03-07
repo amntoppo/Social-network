27e03c4b9c05269937b095914d50395da33c2dba
root <amntoppo@gmail.com>
2018-03-07 21:50:42 +0530

    Updated

--- app.js
+++ app.js
@@ -4,15 +4,34 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expresshbs = require('express-handlebars');
+var session = require('express-session');;
+var mongoStore = require('connect-mongo')(session);
+var flash = require('connect-flash');
+var passport = require('passport');
+var validator = require('express-validator');
+require('./config/passport');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/shopping', {
+    //useMongoClient: true
+});
+
+var db = mongoose.connection;
+
+//db.on('error', console.log('Connection Error'));
+db.once('open', function() {
+    console.log('we are connected');
+});
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var users = require('./routes/user');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine('.hbs', expresshbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,27 +39,44 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+    secret: 'asecretsession',
+    resave: false,
+    saveUninitialized: false,
+    store: new mongoStore({mongooseConnection: db}),
+    cookie: {maxAge: 180 * 60 * 1000}
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(validator());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+    res.locals.login = req.isAuthenticated();
+    res.locals.session = req.session;
+    next();
+});
+
 app.use('/', index);
-app.use('/users', users);
+app.use('/user', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
---
+++ models/user.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+const Schema = mongoose.Schema;
+
+const userSchema = mongoose.Schema({
+   email: {type: String,
+   required: true},
+    password: {type: String, required: true}
+});
+
+userSchema.methods.encryptPassword = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(5), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model('userData', userSchema);
